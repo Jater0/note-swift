11886346dad95b13e4873e5a7ebe45cdfcefb92f
Jater <1452606865@qq.com>
2021-08-16 13:20:43 +0800

    Certificate Card UI

---
+++ SwiftUI-DesignCode/SwiftUI_DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DesignCodeApp.swift
+//  SwiftUI-DesignCode
+//
+//  Created by Jater on 2021/8/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
